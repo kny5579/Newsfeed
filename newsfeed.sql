8923640286ca869ce03eddced1b20b8412d44027
나연 <kny5579@naver.com>
2025-02-14 19:19:54 +0900

    chore: sql 수정

--- newsfeed.sql
+++ newsfeed.sql
@@ -1,22 +1,22 @@
 CREATE TABLE user
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
+    name VARCHAR(20) NOT NULL,
     image_url VARCHAR(1000),
     email VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
-    created_date DATETIME,
-    updated_date DATETIME,
-    deleted BOOLEAN DEFAULT FALSE
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME
 );
 
 CREATE TABLE board
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    contents VARCHAR(200) NOT NULL,
+    contents MEDIUMTEXT NOT NULL,
     image_url VARCHAR(1000) NOT NULL,
-    created_date DATETIME,
-    updated_date DATETIME,
+    created_at DATETIME,
+    updated_at DATETIME,
     user_id BIGINT,
     foreign key (user_id) references user(id) ON DELETE CASCADE
 );
@@ -25,35 +25,39 @@ CREATE TABLE follow
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     follower_id BIGINT NOT NULL,
-    following_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    status ENUM('PENDING','ACCEPTED','REJECTED') DEFAULT 'PENDING',
     foreign key (follower_id) references user(id) ON DELETE CASCADE,
-    foreign key (following_id) references user(id) ON DELETE CASCADE,
-    UNIQUE KEY unique_follow (follower_id, following_id)
+    foreign key (user_id) references user(id) ON DELETE CASCADE,
+    UNIQUE KEY unique_follow (follower_id, user_id)
 );
 
 CREATE TABLE comment
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     contents VARCHAR(200) NOT NULL,
-    created_date DATETIME,
-    updated_date DATETIME,
+    created_at DATETIME,
+    updated_at DATETIME,
     user_id BIGINT,
     board_id BIGINT,
     foreign key (user_id) references user(id) ON DELETE CASCADE,
     foreign key (board_id) references board(id) ON DELETE CASCADE
 );
 
-CREATE TABLE likes
+CREATE TABLE comment_likes
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT,
-    board_id BIGINT NULL,
-    comment_id BIGINT NULL,
+    comment_id BIGINT,
     foreign key (user_id) references user(id) ON DELETE CASCADE,
-    foreign key (board_id) references board(id) ON DELETE CASCADE,
-    foreign key (comment_id) references comment(id) ON DELETE CASCADE,
-    CONSTRAINT check_like CHECK (     -- 둘 중 하나는 null
-        (board_id IS NOT NULL AND comment_id IS NULL) OR
-        (board_id IS NULL AND comment_id IS NOT NULL)
-        )
+    foreign key (comment_id) references comment(id) ON DELETE CASCADE
+);
+
+CREATE TABLE board_likes
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT,
+    board_id BIGINT,
+    foreign key (user_id) references user(id) ON DELETE CASCADE,
+    foreign key (board_id) references board(id) ON DELETE CASCADE
 );
