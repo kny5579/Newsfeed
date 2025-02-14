279beb17fd91cb62300ec203a08267aa5b1139be
Doritos <mydjkim@naver.com>
2025-02-14 20:06:10 +0900

    Feat: BaseEntity, UserEntity 추가

---
+++ src/main/java/com/example/newsfeed/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.newsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "user")
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String img_url;
+
+    private String email;
+
+    private String password;
+
+    private boolean deleted = false;
+}
