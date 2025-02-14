a4d9138851de98c0d80761788047a28642912c09
나연 <kny5579@naver.com>
2025-02-14 15:57:15 +0900

    chore: sql 수정

--- newsfeed.sql
+++ newsfeed.sql
@@ -3,11 +3,11 @@ CREATE TABLE user
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     image_url VARCHAR(1000),
-    email VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     created_date DATETIME,
     updated_date DATETIME,
-    deleted BOOLEAN
+    deleted BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE board
