948a596f730a0c70e86ae4d3551d085f6a8c724a
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-02-17 21:48:42 +0900

    폴더 이동

--- src/main/java/com/example/newsfeed/repository/user/UserRepository.java
+++ src/main/java/com/example/newsfeed/repository/user/UserRepository.java
@@ -1,10 +1,12 @@
-package com.example.newsfeed.repository.user;
+package com.example.newsfeed.user.repository;
 
 import com.example.newsfeed.entity.userEntity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
 }
-
