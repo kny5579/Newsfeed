ead66982306646cf2c49ab6dfbef9df0d8895e2e
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-02-18 11:26:03 +0900

    Revert "git merge"
    This reverts commit 7806af93a881ea126659443b4a645b26e273d944.

---
+++ src/main/java/com/example/newsfeed/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.newsfeed.entity;
+
+import com.example.newsfeed.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+@NoArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean deleted = false;
+
+    private User(Long id) {
+        this.id = id;
+    }
+
+    public User(String name, String imgUrl, String email, String password) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.email = email;
+        this.password = password;
+        this.deleted = false;
+    }
+}
