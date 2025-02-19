539f6c50de087e84d9f9d29482e8687f644192f3
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-02-19 20:38:01 +0900

    JWT

--- src/main/java/com/example/newsfeed/common/utill/JwtUtil.java
+++ src/main/java/com/example/newsfeed/common/utill/JwtUtil.java
@@ -4,13 +4,15 @@ import com.example.newsfeed.common.exception.ForbiddenException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "1234";
+    @Value("${jwt.key}")
+    private String jwtKey;
     private final long EXPIRATION_TIME = 1000 * 60 * 60;
 
     public String generateToken(Long userId) {
@@ -18,7 +20,7 @@ public class JwtUtil {
                 .setSubject(String.valueOf(userId))
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, jwtKey)
                 .compact();
     }
 
@@ -44,7 +46,7 @@ public class JwtUtil {
     }
 
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return Jwts.parser().setSigningKey(jwtKey).parseClaimsJws(token).getBody();
     }
 
     private boolean isTokenExpired(String token) {
