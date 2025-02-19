be00c7ef18daf9e1ba700152b5fc981bfd111044
wonchaebin <usan119@naver.com>
2025-02-19 15:48:36 +0900

    Fix: 중복된 코드 삭제

--- src/main/java/com/example/newsfeed/common/exception/InvalidPasswordFormatException.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsfeed.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class InvalidPasswordFormatException extends ApplicationException{
-    public InvalidPasswordFormatException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
-    }
-}
--- src/main/java/com/example/newsfeed/common/exception/SamePasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.example.newsfeed.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class SamePasswordException extends ApplicationException {
-    public SamePasswordException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
-    }
-}
