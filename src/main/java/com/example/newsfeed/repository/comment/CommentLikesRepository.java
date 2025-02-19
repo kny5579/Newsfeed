06c31b9f8fe25968a5b97968782ea99b71b9c142
Doritos <mydjkim@naver.com>
2025-02-19 15:17:56 +0900

    Refactor: 불필요 코드 삭제

--- src/main/java/com/example/newsfeed/repository/comment/CommentLikesRepository.java
+++ src/main/java/com/example/newsfeed/repository/comment/CommentLikesRepository.java
@@ -1,9 +1,7 @@
 package com.example.newsfeed.repository.comment;
 
 import com.example.newsfeed.dto.comment.CommentLikesDto;
-import com.example.newsfeed.entity.comment.Comment;
 import com.example.newsfeed.entity.comment.CommentLikes;
-import com.example.newsfeed.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,11 +12,10 @@ import java.util.Optional;
 @Repository
 public interface CommentLikesRepository extends JpaRepository<CommentLikes, Long> {
 
-    @Query("select new com.example.newsfeed.dto.comment.CommentLikesDto(c.comment.id, count(c), " +
-            "CASE WHEN SUM(CASE WHEN c.user.id = :userId THEN 1 ELSE 0 END ) > 0 THEN true ELSE  false  END ) " +
+    @Query("select new com.example.newsfeed.dto.comment.CommentLikesDto(c.comment.id, count(c)) " +
             "from CommentLikes c " +
             "where c.comment.id in :commentIds " +
             "group by c.comment.id")
-    List<CommentLikesDto> countByCommentIds(Long userId, List<Long> commentIds);
+    List<CommentLikesDto> countByCommentIds(List<Long> commentIds);
     Optional<CommentLikes> findByCommentIdAndUserId(Long commentId, Long userId);
 }
